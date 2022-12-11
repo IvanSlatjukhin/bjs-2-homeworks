584b92cfb57c7bc7ce9d2f94273d9ddfbacd93dd
IvanSlatjukhin <slatjukhin@mail.ru>
2022-12-11 13:37:20 +0300

    Commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let D=b**2-4*a*c;
+
+  if (D==0){
+    let x1=-b/(2*a);
+    arr=[x1];
+
+  } else if (D>0){
+    let x1=(-b + Math.sqrt(D) )/(2*a);
+    let x2=(-b - Math.sqrt(D) )/(2*a);
+    arr=[x1,x2];
+
+  } else if (D<0){
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  isNaN(percent);
+  isNaN(contribution);
+  isNaN(amount);
+  isNaN(countMonths);
+  let P = percent/100/12;
+  let S = amount - contribution;
+  let payment = S * (P + (P / (((1 + P)**countMonths) - 1)));
+  alert( payment.toFixed(2) );
 }
