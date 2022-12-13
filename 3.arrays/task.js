3a454d375bcbcede59ede167b7c257d532d9611b
IvanSlatjukhin <slatjukhin@mail.ru>
2022-12-13 20:14:20 +0300

    ARR

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length === arr2.length)
+    return arr1.every((element, index) => element === arr2[index]);
+  else return false;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  return users
+    .filter(user => user.gender === gender)
+    .map(student => student.age)
+    .reduce((summ, userAge, index, array) => {
+        if(index === array.length - 1) {
+            return (summ + userAge) / array.length;
+        }
+        return summ + userAge;
+    }, 0);
 }
