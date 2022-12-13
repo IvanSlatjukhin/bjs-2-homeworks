60a110b8e7533e9f8e7e0ed85e163b178ec2fc31
IvanSlatjukhin <slatjukhin@mail.ru>
2022-12-14 00:32:18 +0300

    New com

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,29 +1,36 @@
-"use strict"
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  let D=b**2-4*a*c;
+   let arr = [];
+   let D=b**2-4*a*c;
 
-  if (D==0){
-    let x1=-b/(2*a);
-    arr=[x1];
+   if (D==0){
+     let x1=-b/(2*a);
+     arr=[x1];
 
-  } else if (D>0){
-    let x1=(-b + Math.sqrt(D) )/(2*a);
-    let x2=(-b - Math.sqrt(D) )/(2*a);
-    arr=[x1,x2];
-
-  } else if (D<0){
-  }
-  return arr;
-}
+   } else if (D>0){
+     let x1=(-b + Math.sqrt(D) )/(2*a);
+     let x2=(-b - Math.sqrt(D) )/(2*a);
+     arr=[x1,x2];
+   }
+   return arr;
+ }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  isNaN(percent);
-  isNaN(contribution);
-  isNaN(amount);
-  isNaN(countMonths);
-  let P = percent/100/12;
-  let S = amount - contribution;
-  let payment = S * (P + (P / (((1 + P)**countMonths) - 1)));
-  alert( payment.toFixed(2) );
+
+    let S = amount - contribution;
+    let P = percent / 100 / 12;
+    let date1 = new Date();
+    let n = (date.getFullYear()*12 + date.getMonth()) - (date1.getFullYear()*12 + date1.getMonth());
+
+    if ((percent < 0 ||  contribution < 0 || amount < 0 || n < 0 ) ||
+    (isNaN(percent) === true|| isNaN(contribution) === true || isNaN(amount) === true || isNaN(countMonths) === true)) {
+        return "Ошибка данные введены неверно";
+    }
+
+    let monthAmount = S * ( P + P / (((1 + P) ** n) - 1));
+    let totalAmount = monthAmount * n;
+
+    return totalAmount.toFixed(2);
+
 }
