ee407c892f525c1918c7990d3adac3ebda84f8db
IvanSlatjukhin <slatjukhin@mail.ru>
2022-12-14 23:24:50 +0300

    New com2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,15 +2,15 @@
 
 function solveEquation(a, b, c) {
    let arr = [];
-   let D=b**2-4*a*c;
+   let d=b**2-4*a*c;
 
-   if (D==0){
+   if (d==0){
      let x1=-b/(2*a);
      arr=[x1];
 
-   } else if (D>0){
-     let x1=(-b + Math.sqrt(D) )/(2*a);
-     let x2=(-b - Math.sqrt(D) )/(2*a);
+   } else if (d>0){
+     let x1=(-b + Math.sqrt(d) )/(2*a);
+     let x2=(-b - Math.sqrt(d) )/(2*a);
      arr=[x1,x2];
    }
    return arr;
@@ -20,17 +20,15 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 
     let S = amount - contribution;
     let P = percent / 100 / 12;
-    let date1 = new Date();
-    let n = (date.getFullYear()*12 + date.getMonth()) - (date1.getFullYear()*12 + date1.getMonth());
 
-    if ((percent < 0 ||  contribution < 0 || amount < 0 || n < 0 ) ||
-    (isNaN(percent) === true|| isNaN(contribution) === true || isNaN(amount) === true || isNaN(countMonths) === true)) {
-        return "Ошибка данные введены неверно";
+    if ((percent < 0 || contribution < 0 || amount < 0 || countMonths < 0 ) ||
+    (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths) )) {
+    return false;
     }
 
-    let monthAmount = S * ( P + P / (((1 + P) ** n) - 1));
-    let totalAmount = monthAmount * n;
+    let monthAmount = S * (P + (P / (((1 + P)**countMonths) - 1)));
+    let totalAmount = monthAmount * countMonths;
 
-    return totalAmount.toFixed(2);
+    return +totalAmount.toFixed(2);
 
 }
